a3b4b1402c629bc961226e9363fbb7ca42bcce52
Dana <arrubladana@gmail.com>
2026-04-19 22:24:56 -0500

    feat: Se crea el codigo service GetShows

---
+++ js/service/service.js
@@ -0,0 +1,32 @@
+import { setState, getState } from "./state.js";
+
+const link = "https://api.tvmaze.com";
+
+// esta funcion me va a obtener los show
+export async function getShows() {
+    setState("loading", true);
+    try {
+        const response = await fetch(`${link}/shows`);
+
+        if (!response.ok) {
+            throw new Error("Error al cargar shows");
+        }
+        //
+        const datosGuardados = await response.json();
+        setState("shows", datosGuardados);
+        //
+        return await datosGuardados;
+    } catch (error) {
+        throw error;
+    }finally{
+        setState("loading", false);
+    }
+    
+}
+// este va a buscar los datos que ya tengo en showsList
+export function getShowFromState(id) {
+    const showsList = getState("shows");
+    return showsList.find((s) => s.id === id);
+}
+
+
