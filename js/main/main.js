7e47bb5b3d3821019af39de4a2231caef53f3260
Janier Leudo <janierleudo@gmail.com>
2026-04-20 20:43:02 -0500

    fix: se corrige el metodo para iniciar el programa

--- js/main/main.js
+++ js/main/main.js
@@ -1,11 +1,2 @@
-import {getShows} from "../service/service.js";
-import {renderPosts} from "../ui/ui.js"
-
-async function init() {
-        const shows = await getShows();
-        renderPosts(shows);
-
-}
-
-init();
-
+import { initUI } from "./ui.js";
+initUI();
